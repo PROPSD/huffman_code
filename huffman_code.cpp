c2ab60cf6a148eb588508b7c177a03326324ebe5
Sanjay Davis <152778940+PROPSD@users.noreply.github.com>
2025-03-04 18:15:20 +0530

    initial commit

--- huffman_code.cpp
+++ huffman_code.cpp
@@ -2,144 +2,99 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
-void gather_string(string);
-class node{
 
-    public:
-        int value;
-        char letter;
-        node * left;
-        node * right;
+class node {
+public:
+    int value;
+    char letter;
+    node* left;
+    node* right;
 
+    node(char letter) {
+        this->value = 1;
+        this->letter = letter;
+        this->left = NULL;
+        this->right = NULL;
+    }
 
-        node(char letter){
-            this->value = 1;
-            this->letter = letter;
-            node * left = NULL;
-            node * right = NULL;
-        }
-
-        node(int value){
-            this->value = value;
-            this->letter = ' ';
-        }
+    node(int value) {
+        this->value = value;
+        this->letter = ' ';
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
 
-class table{
-    public:
-        char letter;
-        string output;
+class table {
+public:
+    char letter;
+    string output;
 
-        table(char letter,string temp){
-            this->letter = letter;
-            this->output = temp;
-        }
+    table(char letter, string temp) {
+        this->letter = letter;
+        this->output = temp;
+    }
 };
 
 vector<node*> charecter;
 vector<table> chart;
-/*
-string modify_name(string &file_name){
-    int l = file_name.length();
-    int pos = -1;
-    for(int i=0;i<l;i++){
-        if (file_name[i] != '.') pos++;
-        else break;
-    }
-    pos++;
-    string temp = "_compressed";
-    for(char it : temp){
-        file_name.insert(file_name.begin() + pos, it);
-        pos++;
-    }
-    cout << file_name;
-    return file_name;
-}
-*/
 
-void get_strings(string file_name) {
-    fstream file(file_name, ios::in);
-    if (!file.is_open()) {
-        cerr << "Error: Could not open file " << file_name << endl;
-        return;
-    }
-
-    char ch;
-    int i=0;
-    string temp;
-    while (file.get(ch)) { 
-        i+=1;
-        temp = temp + ch;        
-    }
-    temp += '\0';
-    cout << "A total of "<< i <<" charecters have been read .";
-    gather_string(temp);
-    file.close();
+void swap_node(node*& a, node*& b) {
+    node* temp = a;
+    a = b;
+    b = temp;
 }
 
-void swap_node(node* &a, node* &b) {
-    node temp =* a;
-    *a =* b;
-   * b = temp;
-}
-
-void sort(){
+void sort_nodes() {
     int l = charecter.size();
-    for(int i=0;i<l;i++){
-        for(int j=0;j<l-i-1;j++){
-            if (charecter[j]->value > charecter[j+1]->value){
-                swap_node(charecter[j],charecter[j+1]);
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < l - i - 1; j++) {
+            if (charecter[j]->value > charecter[j + 1]->value) {
+                swap_node(charecter[j], charecter[j + 1]);
             }
         }
     }
 }
-void single_sort(){
-    int l = charecter.size();
-    for(int i=0;i<l;i++){
-        if (charecter[i]->value > charecter[i+1]->value){
-                swap_node(charecter[i],charecter[i+1]);
-            }
-            else break;
 
+void single_sort() {
+    int l = charecter.size();
+    for (int i = l - 2; i >= 0; i--) {
+        if (charecter[i]->value > charecter[i + 1]->value) {
+            swap_node(charecter[i], charecter[i + 1]);
+        } else break;
     }
 }
 
-void print(){
-    for(auto it : charecter){
+void print() {
+    for (auto it : charecter) {
         cout << it->letter << "  " << it->value << endl;
     }
 }
 
-
-node * tree(){
-    sort();
-    while(charecter.size() > 1){
-        
+node* tree() {
+    sort_nodes();
+    while (charecter.size() > 1) {
         print();
-        node * new_node = new node(charecter[0]->value + charecter[1]->value);
+        node* new_node = new node(charecter[0]->value + charecter[1]->value);
         new_node->left = charecter[0];
-        new_node->right = charecter[1]; 
+        new_node->right = charecter[1];
         charecter.erase(charecter.begin());
         charecter.erase(charecter.begin());
         charecter.push_back(new_node);
         single_sort();
-
     }
     return charecter[0];
 }
 
 void assign_string(node* root, string temp, vector<table>& chart) {
     if (root == NULL) return;
-
-    if (root->letter != ' ') { // Leaf node
+    if (root->left == NULL && root->right == NULL) {
         chart.push_back(table(root->letter, temp));
-        cout << "Assigned code to character [" 
-             << (root->letter == ' ' ? "Space" : string(1, root->letter)) 
-             << "]: " << temp << endl;
-    } else { // Internal node
+        cout << "Assigned code to character [" << root->letter << "]: " << temp << endl;
+    } else {
         assign_string(root->left, temp + '0', chart);
         assign_string(root->right, temp + '1', chart);
     }
@@ -156,116 +111,115 @@ void gather_string(string str) {
             }
         }
         if (!found) {
-            cout << "New character: [" << (it == ' ' ? "Space" : string(1, it)) << "]" << endl;
             charecter.push_back(new node(it));
         }
     }
 }
 
-
-node * start(){
-    sort();
-    node * root = tree();
-    assign_string(root,"", chart);
-
-    for(auto it : chart){
-        if (it.letter == '\n'){
-            cout << " \n " << it.output << endl;
-            continue;   
-        }
-        cout << it.letter << " : " << it.output << endl;
-    }
+node* start() {
+    sort_nodes();
+    node* root = tree();
+    assign_string(root, "", chart);
     return root;
 }
 
 stringstream print_strings(string file_name) {
     fstream initial(file_name, ios::in);
     stringstream output;
-
     if (!initial.is_open()) {
         cerr << "Error opening file!" << endl;
         return output;
     }
-
     char l;
     while (initial.get(l)) {
-        bool found = false;
         for (auto& it : chart) {
             if (it.letter == l) {
                 output << it.output;
-                found = true;
                 break;
             }
-        }
-        if (!found) {
-            cerr << "Error: Character [" 
-                 << (l == ' ' ? "Space" : string(1, l)) 
-                 << "] not found in chart!" << endl;
         }
     }
     initial.close();
     return output;
 }
 
-
-int binary(string temp) {
-    int sum = 0;
-    int len = temp.length();
-
-    for (int i = 0; i < len; ++i) {
-        if (temp[len - 1 - i] == '1') {
-            sum += (1 << i); 
-        }
-    }
-    return sum;
-}
-void store_metadata(){
+void store_metadata() {
     string name = "metadata.txt";
-    fstream file(name,ios::app);
-    for(auto it : chart){
-        file << it.letter <<":"<< it.output << endl;
+    fstream file(name, ios::out);
+    for (auto it : chart) {
+        file << it.letter << ":" << it.output << endl;
     }
     file.close();
 }
 
-stringstream convert_binary(stringstream &output){
-    
+stringstream convert_binary(stringstream& output) {
     stringstream total;
     char l;
-    string temp;
-    while (output.get(l))
-    {
+    while (output.get(l)) {
         total << static_cast<char>(l);
     }
     return total;
 }
 
-void send_text(stringstream &str,string file_name){
-    fstream file(file_name,ios::out);
+void send_text(stringstream& str, string file_name) {
+    fstream file(file_name, ios::out);
     char l;
-    while(str.get(l)){
+    while (str.get(l)) {
         file << l;
     }
     file.close();
 }
 
+void help() {
+    cout << "Usage:\n";
+    cout << "  -p <file_path> : Compress a file\n";
+    cout << "  -t <text>      : Compress input text\n";
+    cout << "Extracted file will be uploaded to zipped.bin\n";
+}
 
-int main(){
-
-    string file_name;
-    //cout << "\nEnter the file name with extension (test.txt) to compress : ";
-    file_name = "test.txt";
-    //getline(cin,file_name);
-    get_strings(file_name);
-    node * root = start();
+void with_file(string file_name) {
+    fstream file(file_name, ios::in);
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << file_name << endl;
+        return;
+    }
+    stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+    gather_string(buffer.str());
+    start();
     stringstream output = print_strings(file_name);
     stringstream str = convert_binary(output);
-    //file_name = modify_name(file_name);
-    file_name = "zipped.bin";
-    send_text(str,file_name);
+    send_text(str, "zipped.bin");
     store_metadata();
+}
 
+void with_text(string text) {
+    gather_string(text);
+    start();
+    stringstream output;
+    for (char c : text) {
+        for (auto& it : chart) {
+            if (it.letter == c) {
+                output << it.output;
+                break;
+            }
+        }
+    }
+    stringstream str = convert_binary(output);
+    send_text(str, "zipped.bin");
+    store_metadata();
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        help();
+        return 0;
+    }
+    string option = argv[1];
+    if (option == "-p") with_file(argv[2]);
+    else if (option == "-t") with_text(argv[2]);
+    else help();
     return 0;
-
     
 }
